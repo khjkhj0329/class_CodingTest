0e777463ea5914514e00375654dc23e808a26880
효정이 <w2029@e-mirim.hs.kr>
2021-11-01 10:21:40 +0900

    해당단어 카운트 완성

--- 기초 알고리즘/해당단어 카운트.py
+++ 기초 알고리즘/해당단어 카운트.py
@@ -1,17 +1,26 @@
 # 6.어떤 문자열에 해당단어가 몇개있는지 카운팅
 # ex) "ILOVKEFLOVEE"에서 "LOV"가 몇개 있는지 카운팅
-text = 'ILOVKEFLOVEE'
-text2 = 'LOV'
+text = "ILOVKEFLOVEE"
+text2 = "LOV"
+
+print(text.count(text2))
 count = 0
 for i in range(len(text)):
-    # 첫 글자가 "L"인 경우
-    if text[i] == "L":
-        # 첫 글자가 "O"인 경우
-        if text[i+1] == "O":
-            # 첫 글자가 "V"인 경우
-            if text[i+2] == "V":
-                #카운팅
+    # 첫 글자가 "L"인 경우 and 찾아야 하는 끝 글자의 범위체크
+    if text[i] == text2[0] and i + len(text2) <= len(text):
+        # 두번째 글자가 "O"인 경우
+        if text[i+1] == text2[1]:
+            # 세번째 글자가 "V"인 경우
+            if text[i+2] == text2[2]:
                 count += 1
 print(count)
+    # # 첫 글자가 "L"인 경우
+    # if text[i] == "L":
+    #     # 첫 글자가 "O"인 경우
+    #     if text[i+1] == "O":
+    #         # 첫 글자가 "V"인 경우
+    #         if text[i+2] == "V":
+    #             #카운팅
+    #             count += 1
 
 
