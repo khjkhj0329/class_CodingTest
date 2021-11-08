6880e97ec463fc8eed00dc885fbfb4e909ea2d3a
효정이 <w2029@e-mirim.hs.kr>
2021-11-08 09:52:46 +0900

    enumerate를 사용하는 반복문(선생님과 함께)

--- 기초 알고리즘/enumerate를 사용하는 반복문.py
+++ 기초 알고리즘/enumerate를 사용하는 반복문.py
@@ -1,3 +1,11 @@
 name = ['First', 'Second', 'Third']
 for i, num in enumerate(name):
     print('{}번호는 {}입니다.'.format(i, num))
+print('-- 선생님이 하신거 --')
+arr = [10, 20, 30]
+for x, v in enumerate(arr): #값에 해당하는 인덱스를 알고 싶을 때 range(len)을 쓴다.
+    print(x, v)
+print('-- 함수를 쓰지 않고 --')
+for y in range(len(arr)):
+    print(i, arr[y])
+
