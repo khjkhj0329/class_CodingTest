86720ae3b464be2728f4b0a44c75bc3823311b17
효정이 <w2029@e-mirim.hs.kr>
2021-11-08 09:17:24 +0900

    리스트에서 특정 원소만 추출하기(선생님이랑 함께)변수명 수정

--- 기초 알고리즘/리스프에서 특정 원소만 추출하기.py
+++ 기초 알고리즘/리스프에서 특정 원소만 추출하기.py
@@ -1,10 +1,10 @@
 # ex) [1, 2, 2, 2, 3, 1, 1, 3, 2] 에서  2를 제외한 나머지만 추출하기
 arr = [1, 2, 2, 2, 3, 1, 1, 3, 2]
 
-새로운공간 = list()
+result = list()
 for i in arr:
     if i != 2:
         #print(i)
         # 새로운 공간에 i를 넣는다.
-        새로운공간.append(i)
-print(새로운공간)
+        result.append(i)
+print(result)
